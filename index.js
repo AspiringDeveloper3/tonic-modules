c78a40cb122680b6a812d573060d43849ae680d1
Vedant Saini <72696670+AspiringDeveloper3@users.noreply.github.com>
2021-03-17 15:10:56 +0530

    longest word in a string method

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var repeat = function (timesToRepeat, callback) {
-  for (var newVariable = 0; newVariable < timesToRepeat; newVariable++) {
-    callback(newVariable);
+const repeat = function (timesToRepeat, callback) {
+  for (
+    const newconstiable = 0;
+    newconstiable < timesToRepeat;
+    newconstiable++
+  ) {
+    callback(newconstiable);
   }
 };
-var hasClass = function (itemToCheck, classToCheck, callback) {
+const hasClass = function (itemToCheck, classToCheck, callback) {
   callback = callback || null;
   itemToCheck.classList.forEach(function (clas) {
     if (clas === classToCheck) {
@@ -17,65 +21,65 @@ var hasClass = function (itemToCheck, classToCheck, callback) {
     }
   });
 };
-var setCss = function (item, cssProp, value) {
+const setCss = function (item, cssProp, value) {
   item.style[cssProp] = value;
 };
-var tn = function (query) {
-  var selected = document.querySelectorAll(query);
+const tn = function (query) {
+  const selected = document.querySelectorAll(query);
   if (selected.length > 1) {
     return document.querySelectorAll(query);
   } else {
     return document.querySelector(query);
   }
 };
-var setText = function (element, newText) {
+const setText = function (element, newText) {
   element.innerHTML = newText;
 };
-var blink = function (element, timeout) {
+const blink = function (element, timeout) {
   element.style.transition = "all 0.8s ease";
   element.style.opacity = "0";
   setTimeout(function () {
     element.style.opacity = "1";
   }, timeout);
 };
-var click = function (element, callback) {
+const click = function (element, callback) {
   element.addEventListener("click", function () {
     return callback();
   });
 };
-var keypress = function (element, callback) {
+const keypress = function (element, callback) {
   element.addEventListener("keypress", function (event) {
     return callback(event.key);
   });
 };
-var fadeIn = function (element) {
+const fadeIn = function (element) {
   element.style.opacity = "1";
   element.style.transition = "all 0.6s ease-in-out";
 };
-var fadeOut = function (element) {
+const fadeOut = function (element) {
   element.style.opacity = "0";
   element.style.transition = "all 0.6s ease-in-out";
 };
 
 // Array methods
 
-var contains = function (arr, v) {
-  for (var i = 0; i < arr.length; i++) {
+const contains = function (arr, v) {
+  for (const i = 0; i < arr.length; i++) {
     if (arr[i] === v) return true;
   }
   return false;
 };
-var unique = function (target) {
-  var arr = [];
-  for (var i = 0; i < target.length; i++) {
+const unique = function (target) {
+  const arr = [];
+  for (const i = 0; i < target.length; i++) {
     if (!contains(arr, target[i])) {
       arr.push(target[i]);
     }
   }
   return arr;
 };
-var strings = function (target) {
-  var arr = [];
+const strings = function (target) {
+  const arr = [];
   target.forEach(function (value) {
     if (typeof value == "string") {
       arr.push(value);
@@ -83,8 +87,8 @@ var strings = function (target) {
   });
   return arr;
 };
-var numbers = function (target) {
-  var arr = [];
+const numbers = function (target) {
+  const arr = [];
   target.forEach(function (value) {
     if (typeof value == "number") {
       arr.push(value);
@@ -92,17 +96,30 @@ var numbers = function (target) {
   });
   return arr;
 };
-var kebabCase = function (string) {
+
+// String methods
+const longestWord = (string) => {
+  let longestWord = string.split(" ")[0];
+  let longestWordLength = longestWord.length;
+  string.split(" ").forEach((word) => {
+    if (word.length > longestWordLength) {
+      longestWord = word;
+      longestWordLength = word.length;
+    }
+  });
+  return longestWord;
+};
+const kebabCase = function (string) {
   return string.toLowerCase().split(" ").join("-");
 };
-var snakeCase = function (string) {
+const snakeCase = function (string) {
   return string.toLowerCase().split(" ").join("_");
 };
-var titleCase = function (str) {
-  var result = [];
+const titleCase = function (str) {
+  const result = [];
   str.split(" ").forEach(function (word) {
-    var capitalLetter = word.split("").splice(0, 1).join("").toUpperCase();
-    var smallLetters = word.split("").splice(1, word.length).join("");
+    const capitalLetter = word.split("").splice(0, 1).join("").toUpperCase();
+    const smallLetters = word.split("").splice(1, word.length).join("");
     result.push(capitalLetter + smallLetters);
   });
   return result.join(" ");
@@ -127,4 +144,5 @@ module.exports = {
   setText,
   repeat,
   hasClass,
+  longestWord,
 };
